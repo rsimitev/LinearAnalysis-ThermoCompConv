0f7b58ef6a297a458f142b10e3c4148fa96d3e70
Luis Silva <Luis.Silva@glasgow.ac.uk>
2014-11-10 15:08:03 +0000

    Cleanup the parameters module.

--- DoubleDiffConv/parameters.f90
+++ DoubleDiffConv/parameters.f90
@@ -1,27 +1,23 @@
 module parameters
   implicit none
   ! Adimensional parameters
-  double precision:: TAU,RA,PR,RI,C,pL,Rconc,eta
+  double precision:: TAU,Rt,Pt,RI,C,pL,Rc,eta
   ! Dimensions
   integer::NT,M0,ND,LMIN,LD,NE, NSMAX, LCALC
-  double precision:: TTA,TTF,TTSTEP,DRA,ABSE,RELE
+  double precision:: TTA,TTF,TTSTEP,DRt,ABSE,RELE, DRc
 contains
    subroutine setDefaults()
       implicit none
       NE    = 2
       LCALC = 2
-      RA       = 4.D3
-      RAOLD    = 4.D3
-      DRA      = RA*0.1d0
-      Rconc    = 4.D3
-      RconcOLD = 4.D3
-      DRconc   = Rconc*0.1D0
+      Rt       = 4.D3
+      DRt      = Rt*0.1d0
+      Rc    = 4.D3
+      DRc   = Rc*0.1D0
       TAU  = 100.0D0
-      TAU1 = 100.0D0
-      TAU2 = 100.0D0
       TTA  = TAU
       TTF  = 10*TTA
-      PR   = 1.D-1
+      Pt   = 1.D-1
       pL   = 1.D00
       ETA  = 0.4D0
       NT   = 3
