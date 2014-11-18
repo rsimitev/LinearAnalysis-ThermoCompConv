b208c0eda147c660cb17acf778313a6f9759efaa
Luis Silva <Luis.Silva@glasgow.ac.uk>
2014-11-18 13:59:29 +0000

    Update the mode tracker to take advantage of the fact that we actually have two functions that need to be continuous with a continuous first derivative: the frequency and the growth rate.

--- src/modeTracker/main.f90
+++ src/modeTracker/main.f90
@@ -1,15 +1,17 @@
 !> Reads a file with the number of modes and a list of files
 !! containing said modes for different parameters,
-!! one file per line.
+!! one file per line. For each file, outputs a reordered set
+!! of values based on a quadratic extrapolation through the 
+!! previous 3 files.
 program modeTracker
    implicit none
    integer, parameter:: unitFileList=15, nPoints=3
    character(len=128):: listFileName, fileToRead
    character(len=256):: line
    double precision:: par(nPoints)
-   double precision, allocatable:: modes(:,:), projectedModeVals(:)
+   double complex, allocatable:: modes(:,:), projectedModeVals(:)
    double precision:: newPar
-   double precision, allocatable:: newModeSet(:)
+   double complex, allocatable:: newModeSet(:)
    integer:: nModes !< the number of modes to be read from each file
    integer:: nFile !< The index of the modes file read
    integer:: info, i, j
@@ -24,6 +26,7 @@ program modeTracker
       nFile = nFile + 1
       ! Shift the parameter and mode pointers to the next value
       read(line,*) newPar, fileToRead
+      Write(*,*) "Processing ", trim(fileToRead)
       ! Read one file at a time
       call readModesFile(trim(fileToRead), newModeSet)
       ! For each mode, we compute the projected mode value
@@ -41,22 +44,7 @@ program modeTracker
                ! if j>i always swap
                if (j>i) then
                   call swap(newModeSet(i),newModeSet(j))
-               else !if j<i swap only when it makes a difference to both.
-                  if(abs(projectedModeVals(j)-newModeSet(i)).lt. &
-                     abs(projectedModeVals(j)-newModeSet(j))) then
-                     call swap(newModeSet(i),newModeSet(j))
-                  endif
-               endif
-            endif
-         enddo
-         ! Take a second pass?
-         do j=1, nModes
-            if(abs(projectedModeVals(i)-newModeSet(j)).lt. &
-               abs(projectedModeVals(i)-newModeSet(i))) then
-               ! if j>i always swap
-               if (j>i) then
-                  call swap(newModeSet(i),newModeSet(j))
-               else !if j<i swap only when it makes a difference to both.
+               else !if j<i, swap only when it makes a difference to both.
                   if(abs(projectedModeVals(j)-newModeSet(i)).lt. &
                      abs(projectedModeVals(j)-newModeSet(j))) then
                      call swap(newModeSet(i),newModeSet(j))
@@ -105,10 +93,10 @@ contains
       do
          call readLineOfFilesToRead(unitFileList, line, info)
          if(info.ne.0) exit
-         Write(*,*) line
          ! Read one file at a time
          nFile = nFile + 1
          read(line,*) par(nFile), fileToRead
+         Write(*,*) "Processing ", trim(fileToRead)
          call readModesFile(fileToRead, modes(:,nFile))
          call saveReorderedModeSet(modes(:,nFile), nModes)
          if (nFile==nPoints) exit
@@ -139,7 +127,7 @@ contains
    subroutine readModesFile(fileToRead, modes)
       implicit none
       character(len=*), intent(in):: fileToRead
-      double precision, intent(out):: modes(:)
+      double complex, intent(out):: modes(:)
       character(len=128):: line
       double precision:: modeVal, modeFreq
       integer:: n, readStatus
@@ -159,7 +147,7 @@ contains
          ! Read the mode index and the value
          read(line,*) n, modeFreq, modeVal
          if (n.gt.nModes) exit
-         modes(n) = modeVal
+         modes(n) = cmplx(modeFreq, modeVal)
       enddo
 
       close(unit=20)
@@ -170,9 +158,11 @@ contains
    !**********************************************************************
    function nextPoint3(xOld,yOld,x) result(y)
       implicit none
-      double precision:: y
-      double precision, intent(in):: xOld(nPoints), yOld(nPoints), x
-      double precision:: a,b,c,d, x1, x2, x3, y1, y2, y3
+      double complex:: y
+      double precision, intent(in):: xOld(nPoints), x
+      double complex, intent(in):: yOld(nPoints)
+      double precision:: d, x1, x2, x3
+      double complex:: a,b,c,y1, y2, y3
 
       x1 = xOld(1)
       x2 = xOld(2)
@@ -189,75 +179,14 @@ contains
       y = a*x**2 + b*x + c
    end function
 
-#ifdef CUBIC
-   !**********************************************************************
-   !> Given four points of coordinates xOld, yOld, extimate the next point at x.
-   !**********************************************************************
-   function nextPoint4(xOld,yOld,x) result(y)
-      implicit none
-      double precision:: y
-      double precision, intent(in):: xOld(nPoints), yOld(nPoints), x
-      double precision:: a,b,c,d, x1, x2, x3, x4, y1, y2, y3, y4
-
-      x1 = xOld(1)
-      x2 = xOld(2)
-      x3 = xOld(3)
-      x4 = xOld(4)
-      y1 = yOld(1)
-      y2 = yOld(2)
-      y3 = yOld(3)
-      y4 = yOld(4)
-
-      a = ( ((x2-x1)*x3**2+(x1**2-x2**2)*x3+x1*x2**2-x1**2*x2)*y4 + &
-            ((x1-x2)*x4**2+(x2**2-x1**2)*x4-x1*x2**2+x1**2*x2)*y3 + &
-            ((x3-x1)*x4**2+(x1**2-x3**2)*x4+x1*x3**2-x1**2*x3)*y2 + &
-            ((x2-x3)*x4**2+(x3**2-x2**2)*x4-x2*x3**2+x2**2*x3)*y1 )/( &
-            ((x2-x1)*x3**2+(x1**2-x2**2)*x3+x1*x2**2-x1**2*x2)*x4**3 + &
-            ((x1-x2)*x3**3+(x2**3-x1**3)*x3-x1*x2**3+x1**3*x2)*x4**2 + &
-            ((x2**2-x1**2)*x3**3+(x1**3-x2**3)*x3**2+x1**2*x2**3-x1**3*x2**2)*x4 + &
-             (x1**2*x2-x1*x2**2)*x3**3 + &
-             (x1* x2**3-x1**3*x2)*x3**2 + &
-             (x1**3*x2**2-x1**2*x2**3)*x3 )
-      b = -( ((x2-x1)*x3**3 + (x1**3-x2**3)*x3 + x1*x2**3-x1**3*x2)*y4 + &
-             ((x1-x2)*x4**3 + (x2**3-x1**3)*x4 - x1*x2**3+x1**3*x2)*y3 + &
-             ((x3-x1)*x4**3 + (x1**3-x3**3)*x4 + x1*x3**3-x1**3*x3)*y2 + &
-             ((x2-x3)*x4**3 + (x3**3-x2**3)*x4 - x2*x3**3+x2**3*x3)*y1 )/( &
-             ((x2-x1)*x3**2 + (x1**2-x2**2)*x3 + x1*x2**2-x1**2*x2)*x4**3 + &
-             ((x1-x2)*x3**3 + (x2**3-x1**3)*x3 - x1*x2**3+x1**3*x2)*x4**2 + &
-             ((x2**2-x1**2)*x3**3 + (x1**3-x2**3)*x3**2 + x1**2*x2**3 - x1**3*x2**2)*x4 + &
-             (x1**2*x2-x1*x2**2)*x3**3 + &
-             (x1*x2**3-x1**3*x2)*x3**2 + &
-             (x1**3*x2**2-x1**2*x2**3)*x3 )
-      c = ( ((x2**2-x1**2)*x3**3+(x1**3-x2**3)*x3**2+x1**2*x2**3-x1**3*x2**2)*y4 + &
-            ((x1**2-x2**2)*x4**3+(x2**3-x1**3)*x4**2-x1**2*x2**3+x1**3*x2**2)*y3 + &
-            ((x3**2-x1**2)*x4**3+(x1**3-x3**3)*x4**2+x1**2*x3**3-x1**3*x3**2)*y2 + &
-            ((x2**2-x3**2)*x4**3+(x3**3-x2**3)*x4**2-x2**2*x3**3+x2**3*x3**2)* y1 )/( &
-            ((x2-x1)*x3**2+(x1**2-x2**2)*x3+x1*x2**2-x1**2*x2)*x4**3+((x1-x2)*x3**3 + &
-            (x2**3-x1**3)*x3-x1*x2**3+x1**3*x2)*x4**2+((x2**2 -x1**2)*x3**3+ &
-            (x1**3-x2**3)*x3**2+x1**2*x2**3-x1**3*x2**2)*x4+( x1**2*x2-x1*x2**2)*x3**3+&
-            (x1*x2**3-x1**3*x2)*x3**2+(x1**3*x2**2 -x1**2*x2**3)*x3)
-      d = -( ((x1*x2**2-x1**2*x2)*x3**3 + (x1**3*x2-x1 *x2**3)*x3**2 + &
-             (x1**2*x2**3-x1**3*x2**2)*x3)*y4 + ((x1**2*x2-x1*x2 **2)*x4**3+&
-             (x1*x2**3-x1**3*x2)*x4**2+(x1**3*x2**2-x1**2*x2**3)* x4)*y3+&
-             ((x1*x3**2-x1**2*x3)*x4**3+(x1**3*x3-x1*x3**3)*x4**2+&
-             (x1**2*x3**3-x1**3*x3**2)*x4)*y2+((x2**2*x3-x2*x3**2)*x4**3+&
-             (x2*x3 **3-x2**3*x3)*x4**2+(x2**3*x3**2-x2**2*x3**3)*x4)*y1)/(&
-             ((x2-x1) *x3**2+(x1**2-x2**2)*x3+x1*x2**2-x1**2*x2)*x4**3+&
-             ((x1-x2)*x3**3 +(x2**3-x1**3)*x3-x1*x2**3+x1**3*x2)*x4**2+&
-             ((x2**2-x1**2)*x3**3 +(x1**3-x2**3)*x3**2+x1**2*x2**3-x1**3*x2**2)*x4+&
-             (x1**2*x2-x1*x2**2)*x3**3+(x1*x2**3-x1**3*x2)*x3**2+&
-             (x1**3*x2**2-x1**2*x2**3) *x3)
-      y = a*x**3 + b*x**2 + c*x + d
-   end function
-#endif
 
    !**********************************************************************
    !> Swap a with b
    !**********************************************************************
    subroutine swap(a,b)
       implicit none
-      double precision, intent(inout):: a,b
-      double precision:: c
+      double complex, intent(inout):: a,b
+      double complex:: c
       c=a
       a=b
       b=c
@@ -269,11 +198,11 @@ contains
    subroutine saveReorderedModeSet(modeSet, nModes)
       implicit none
       integer, intent(in):: nModes
-      double precision, intent(in):: modeSet(nModes)
+      double complex, intent(in):: modeSet(nModes)
       integer:: i
       open(unit=33, file=trim(fileToRead)//'-sorted', status='NEW')
       do i=1, nModes
-         Write(33,*) i, modeSet(i)
+         Write(33,*) i, dble(modeSet(i)), dimag(modeSet(i))
       enddo
       close(unit=33)
    end subroutine
