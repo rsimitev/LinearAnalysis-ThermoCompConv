39ee3f829afdaeef118e86c4c0e1cb19993cd48a
Luis Silva <Luis.Silva@glasgow.ac.uk>
2015-09-11 14:55:26 +0100

    Fix plotting. It now reproduces the results from the old code.

--- src/plotting/simplePlot.f90
+++ src/plotting/simplePlot.f90
@@ -13,7 +13,8 @@ PROGRAM simplePlot
    implicit none
    double precision, parameter:: PI = 3.14159265358979D0
    integer, parameter:: nr=33, nt=180, np=360
-   CHARACTER*40:: INFILE,OUTFILE
+   CHARACTER(len=60):: INFILE
+   character(len=2):: domain
 
    type:: eigenElement
       integer:: n
@@ -27,34 +28,84 @@ PROGRAM simplePlot
 !---------------------------------------------------------
 !  arg #1 - filename or usage ?
    call getarg(1,infile)
-   if (infile.eq.' ') then
+   if (trim(infile).eq.'' .or. trim(infile).eq.'-h') then
       print*, 'Usage : '
-      print*, 'lara <in file> <out file>'
+      print*, 'simplePlot <in file> <domain>'
+      print*, '  <domain> can be one of 2D or 3D.'
       stop
    endif
-   if (infile.eq.'-h') then
-      print*, 'Usage : '
-      print*, 'lara <in file> <out file>'
-      stop
-   endif
-
-   call getarg(2,outfile)
-   print*,  trim(infile),' - ',trim(outfile)
-
-   write(*,*) 'reading data from ',INFILE,'...'
-   CALL READLA(INFILE)
-   write(*,*) '...done'
    
-   CALL Plot()
+   call getarg(2,domain)
+   print*, 'Will plot ', trim(infile),' in ',domain,'.'
+
+   write(*,*) 'Reading data from ',INFILE,'...'
+   
+   CALL READLA(INFILE)
+   Write(*,*) 'Plotting...'
+   select case (domain)
+      case('2D')
+         CALL Plot_2D()
+      case('3D')
+         CALL Plot_3D()
+   end select
+   Write(*,*) 'Done!'
 
 contains
 
    !------------------------------------------------------------------------
    !     calculates the field Z and makes one subplot.
-   SUBROUTINE Plot()
+   SUBROUTINE Plot_2D()
+      implicit none
+      double precision:: THETA, r, phi
+      double precision:: ri, z
+      integer:: i, j, k
+      !-- CALCULATION OF INNER AND OUTER RADIUS:
+      RI = ETA/(1.D0-ETA)
+
+      write(*,*) 'computing the fields...'
+      ! Plot the equator
+      theta  = 90.0d0
+      
+      ! Find the highest l or m that we need to compute
+      ! Reuse k for that so we don't have to create a new variable
+      k=0
+      DO i=1, size(eigenVector)
+         if (eigenVector(i)%m.gt.k) k = eigenVector(i)%m
+         if (eigenVector(i)%l.gt.k) k = eigenVector(i)%l
+      enddo
+
+      !-- BESTIMMUNG DER PLM(THETA) , ABSPEICHERUNG:
+      
+      CALL STOREPLM( (/THETA/), 1, k )
+
+      open(20,file ='glo-render_2D.general',STATUS =  'UNKNOWN')
+      Write(20,*) 'grid = ',np+1,'x',nr+1
+      Write(20,*) 'format = ascii'
+      Write(20,*) 'interleaving = field'
+      Write(20,*) 'majority = column'
+      Write(20,*) 'field = locations, field0'
+      Write(20,*) 'structure = 2-vector, scalar'
+      Write(20,*) 'type = float, float'
+      Write(20,*) ''
+      Write(20,*) 'end'
+      do i=0, nr
+         r = ri + dble(i)/dble(nr)
+         do k=0, np
+            phi = k*(360.0/np)
+            Write(20,*) r*cos(phi*pi/180.0), &
+                        r*sin(phi*pi/180.0), &
+                        flow_r(eigenVector, r, PHI, 1)
+         enddo
+      enddo
+      close(20)
+   end subroutine Plot_2D
+
+   !------------------------------------------------------------------------
+   !     calculates the field Z and makes one subplot.
+   SUBROUTINE Plot_3D()
       implicit none
       double precision:: THETA(Nt), r, phi, dtheta
-      double precision:: ri, z
+      double precision:: ri
       integer:: i, j, k
       !-- CALCULATION OF INNER AND OUTER RADIUS:
       RI = ETA/(1.D0-ETA)
@@ -78,25 +129,30 @@ contains
       
       CALL STOREPLM( THETA, Nt, k )
 
-      open(20,file ='glo-render.dat',STATUS =  'UNKNOWN')
-      Write(20,*) '#,nt,x,np,x,nr'
-      Write(20,*) '#',nt,'x',np+1,'x',nr+1
-      Write(20,*) '#========================'
+      open(20,file ='glo-render_3D.general',STATUS =  'UNKNOWN')
+      Write(20,*) 'grid = ',nt,'x',np+1,'x',nr+1
+      Write(20,*) 'format = ascii'
+      Write(20,*) 'interleaving = field'
+      Write(20,*) 'majority = column'
+      Write(20,*) 'field = locations, field0'
+      Write(20,*) 'structure = 3-vector, scalar'
+      Write(20,*) 'type = float, float'
+      Write(20,*) ''
+      Write(20,*) 'end'
       do i=0, nr
          r = ri + dble(i)/dble(nr)
          do k=0, np
             phi = k*(360.0/np)
             do j=1, nt
-               Z = flow_r(eigenVector, r, PHI, j)
                Write(20,*) r*sin((theta(j))*pi/180.0)*cos(phi*pi/180.0), &
                            r*sin((theta(j))*pi/180.0)*sin(phi*pi/180.0), &
                            r*cos((theta(j))*pi/180.0), &
-                           z
+                           flow_r(eigenVector, r, PHI, j)
             enddo
          enddo
       enddo
       close(20)
-   end subroutine plot
+   end subroutine Plot_3D
 
    !------------------------------------------------------------------------
    !> Radial flow: U_r = L_2/r v
@@ -104,7 +160,8 @@ contains
       IMPLICIT none
       type(eigenElement)::eigenVector(:)
       double precision, intent(in):: R, phi
-      double precision:: ri, rft, epsm, pphi
+      double precision:: ri, epsm, pphi
+      double precision:: RFT, RFTR, RFTI
       integer, intent(in):: iTheta
       integer:: i
       integer:: l, m, n
@@ -116,6 +173,7 @@ contains
       flow_r = 0.0d0
       DO i=1, size(eigenVector)
          if (eigenVector(i)%fieldCode.ne.'V') cycle
+         ! Prefactor for Legendre Associated Polynomials
          IF( eigenVector(i)%M.EQ.0 ) THEN
             EPSM = 1.D0
          ELSE
@@ -127,10 +185,10 @@ contains
          n = eigenVector(i)%n
          RFT = EPSM*l*(l+1)*PLMS(L,M,iTheta)*DSIN( N*PI*(R-RI) ) / R
 
-         RFT = RFT * eigenVector(I)%realPart * DCOS( M*PPHI )
-         RFT = RFT - RFT * eigenVector(I)%ImagPart * DSIN( M*PPHI )
+         RFTR =  RFT * eigenVector(I)%realPart * DCOS( M*PPHI )
+         RFTI = -RFT * eigenVector(I)%ImagPart * DSIN( M*PPHI )
 
-         flow_r = flow_r + RFT
+         flow_r = flow_r + RFTR+RFTI
       enddo
    end function
 
