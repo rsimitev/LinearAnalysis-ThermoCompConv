b67945fa253b6e7b290e2deabe8e69ed931c9125
Luis Silva <Luis.Silva@glasgow.ac.uk>
2015-09-22 15:49:39 +0100

    Use glo_constants.

--- src/CritRaEff.f90
+++ src/CritRaEff.f90
@@ -7,6 +7,7 @@ program CriticalRaEff
    use parameters
    use GrowthRateMod
    use CritRaEff_io
+   use glo_constants
    implicit none
    type GlobalCrit
       double precision:: alpha
@@ -14,7 +15,6 @@ program CriticalRaEff
       double precision:: w
       integer:: m
    end type
-   double precision, parameter:: DPI=3.141592653589793D0
    integer, parameter:: NN = 3600
    character*60:: infile,outfile
    integer, parameter:: unitOut=16
